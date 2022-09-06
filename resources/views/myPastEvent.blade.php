a0e047edddaab5d0bba22700571b0c6692a3f93d
Guillermo García <guillermieres97@gmail.com>
2022-09-06 09:28:23 +0200

    changes

--- resources/views/myPastEvent.blade.php
+++ resources/views/myPastEvent.blade.php
@@ -31,7 +31,7 @@ $countEventsUser = count($pastEvents);
   @if ($eventOfUser->event_date < now()) <a href="{{ route('showEvent', $eventOfUser->id) }}" class="flex flex-col items-center mx-20 md:flex-row md:max-w-sm">
     <img class="object-cover w-full h-200 rounded-t-sm ms:h-auto sm:w-48 sm:rounded-none md:rounded-none sm" src="{{ $eventOfUser->img }}" alt="Event Image">
 
-    <div id="paragraph" class="flex flex-col justify-between p-4 leading-normal max-w-screen-md">
+    <div id="paragraph" class="justify-between p-4 leading-normal max-w-screen-md">
       <div class="max-w-screen-sm flex justify-between items-center min-w-full bg-gray-400 p-2 mt-4 title">
         <h5 class="w-full text-xl font-bold tracking-tight text-gray-900 dark:text-black uppercase">{{$eventOfUser->title}}</h5>
       </div>
